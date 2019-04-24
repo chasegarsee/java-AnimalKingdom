094e6fff745a0b3ad4afbde13e5ed13a8fb2518d
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-04-24 17:02:31 -0500

    Added Main class with ArrayList and filter logic. Starting on the Lambda Expressions

---
+++ AnimalKingdomAPP/src/animalKingdomApp/Main.java
@@ -0,0 +1,32 @@
+package animalKingdomApp;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] names = { "Panda", "Zebra", "Koala", "Sloth", "Armadillo", "Raccoon", "Bigfoot", "Pigeon", "Peacock",
+                "Toucan", "Parrot", "Swan", "Salmon", "Catfish", "Perch" };
+        String[] types = { "mammals", "mammals", "mammals", "mammals", "mammals", "mammals", "mammals", "birds",
+                "birds", "birds", "birds", "birds", "fish", "fish", "fish" };
+        int[] years = { 1869, 1778, 1816, 1804, 1758, 1758, 2021, 1837, 1821, 1758, 1824, 1758, 1758, 1817, 1758 };
+
+        ArrayList<AbstractAnimal> animalsArrayList = new ArrayList<AbstractAnimal>();
+
+        for (int i = 0; i < names.length; i++) {
+            if (types[i].equals("mammals")) {
+                animalsArrayList.add(new Mammals(names[i], years[i]));
+
+            } else if (types[i].equals("birds")) {
+                animalsArrayList.add(new Birds(names[i], years[i]));
+            } else {
+                animalsArrayList.add(new Fish(names[i], years[i]));
+            }
+        }
+
+        for (AbstractAnimal a : animalsArrayList) {
+            System.out.println(a.getId() + " " + a.getName() + " " + a.getYear() + " " + a.move() + " " + a.reproduce()
+                    + " " + a.breath());
+        }
+        System.out.println("All animals in descending order by the year they were named");
+    }
+}
